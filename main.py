451cd4223fa206d0e09ff0b5f889dd83eb21d72c
Spoiled Unknown <spoiledunknowndev@gmail.com>
2025-07-10 17:38:05 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input("You are on a dirt road, it has come to an end and you can go left or right. which way would you like to go? ").lower()
+
+if answer == "left":
+    answer = input("you come to a river, you can walk around it or swim across? Type walk to walk around or swim to swim: ").lower()
+    if answer == "swim":
+        print("You swam across and were eaten by an aligator")
+    elif answer == "walk":
+        print("You walked for many kilometers, ran out of water and you lost the game.")
+    else:
+        print("Not a valid option. You lose.")
+
+elif answer == "right":
+    answer = input("You come to a dridge, it looks wobbly, do you to cross it or head back (cross/back)? ").lower()
+    if answer == "cross":
+        answer = input("You cross the bridge and meet a stranger. Do you talk to them (yes/no)? ").lower()
+        if answer == "yes":
+            print("You talk to the stranger and they give you the gold. You WIN!")
+        elif answer == "no":
+            print("You ignore the stranger and they are offended and you lose.")
+        else:
+            print("Not a valid option. You lose.")
+    elif answer == "back":
+        print("You go back and lose.")
+    else:
+        print("Not a valid option. You lose.")
+
+else:
+    print("Not a valid option. You lose.")
+
+print("Thank you for trying", name)
